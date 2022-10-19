555efd922c3a72ed7e9e9fdeed6f485b9f50f316
Hielke van der Wal <hielke010@gmail.com>
2022-10-19 11:50:54 +0200

    added autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = 'V0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode', 'autoapi.extension']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
 
 autoapi_type = 'python'
 autoapi_dirs = ['../source']
