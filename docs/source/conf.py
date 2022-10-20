d5dd67138b3e81e2c66d68821aadc5eb1bfe452e
Hielke van der Wal <hielke010@gmail.com>
2022-10-20 11:07:51 +0200

    added napolean to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = 'V0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
               'sphinx.ext.intersphinx',
               'sphinx.ext.viewcode',
               'sphinx.ext.inheritance_diagram',
